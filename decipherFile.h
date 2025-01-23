78c4262c9b4dec19d092d610865f7329d128fa60
mani-hash <manimehalan400@gmail.com>
2025-01-23 23:00:08 +0530

    feat: declaration function

--- decipherFile.h
+++ decipherFile.h
@@ -1,10 +1,12 @@
 #ifndef DECIPHER_FILE_H
 #define DECIPHER_FILE_H
 
+#include <stdbool.h>
 #include "types.h"
 
 int getNoOfNodes();
 char *getNodeList();
+bool isGraphDirected();
 
 Graph *createGraphFromFile();
 

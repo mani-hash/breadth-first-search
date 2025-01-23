7547a9ba2a83a85c90913c2c9ba6d5aef2e4048f
mani-hash <manimehalan400@gmail.com>
2025-01-23 21:27:58 +0530

    feat: graph struct

--- types.h
+++ types.h
@@ -1,10 +1,18 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <stdbool.h>
+
 typedef struct {
     char nodeName;
     int weight;
     GraphNode *next;
 } GraphNode;
+
+typedef struct {
+    int noOfNodes;
+    bool directed;
+    GraphNode **adjacentLists;
+} Graph;
 
 #endif

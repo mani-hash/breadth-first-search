bc3abcac8c7940a0117546659483bc3790ac44af
mani-hash <manimehalan400@gmail.com>
2025-01-24 20:18:30 +0530

    feat: added traversal stats struct

--- logic.h
+++ logic.h
@@ -1,6 +1,13 @@
 #ifndef LOGIC_H
 #define LOGIC_H
 
+typedef struct TraversalStats {
+    size_t nodesVisited;
+    char *traversalPath;
+    size_t totalWeight;
+    char *unReachableNodes;
+} TraversalStats;
+
 void bfsAlgorithm(Graph *graph);
 
 #endif

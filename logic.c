ca19f3bde7c22e6be700e895c5631b959c96539b
mani-hash <manimehalan400@gmail.com>
2025-01-24 23:00:16 +0530

    feat: logic to track traversal path

--- logic.c
+++ logic.c
@@ -10,6 +10,7 @@ static void initTraversalStats(TraversalStats *traversalStats);
 static void enqueue(char value);
 static char dequeue();
 static bool isVisited(char value);
+static void trackTraversalPath(TraversalStats *traversalStats, char value);
 
 char *queue;
 bool *visited;
@@ -33,6 +34,7 @@ static void initVisitedArray(size_t noOfNodes)
 static void initTraversalStats(TraversalStats *traversalStats)
 {
     traversalStats->totalWeight = 0;
+    traversalStats->traversalLength = 0;
     traversalStats->traversalPath = NULL;
     traversalStats->unReachableNodes = NULL;
 }
@@ -68,6 +70,40 @@ static bool isVisited(char value)
     return visited[value - 'A'];
 }
 
+static void trackTraversalPath(TraversalStats *traversalStats, char value)
+{
+    if (traversalStats->traversalPath == NULL)
+    {
+        char *newTraversalPath = (char *)malloc(sizeof(char));
+
+        if (newTraversalPath == NULL)
+        {
+            perror("Failed to allocate memory");
+            exit(EXIT_FAILURE);
+        }
+
+        *newTraversalPath = value;
+        traversalStats->traversalPath = newTraversalPath;
+    }
+    else
+    {
+        char *updatedTraversalPath = (char *)realloc
+        (
+            traversalStats->traversalPath, (traversalStats->traversalLength+1) * sizeof(char)
+        );
+
+        if (updatedTraversalPath == NULL)
+        {
+            perror("Failed to allocate memory");
+            exit(EXIT_FAILURE);
+        }
+
+        updatedTraversalPath[traversalStats->traversalLength] = value;
+        traversalStats->traversalPath = updatedTraversalPath;
+    }
+    traversalStats->traversalLength++;
+}
+
 TraversalStats bfsAlgorithm(Graph *graph)
 {
     TraversalStats traversalStats;
@@ -88,6 +124,7 @@ TraversalStats bfsAlgorithm(Graph *graph)
         if (!isVisited(visitedNodeName))
         {
             visited[visitedNodeName - 'A'] = true;
+            trackTraversalPath(&traversalStats, visitedNodeName);
         }
 
         GraphNode *currentNode = graph->adjacentLists[visitedNodeName - 'A'];
@@ -99,6 +136,7 @@ TraversalStats bfsAlgorithm(Graph *graph)
                 enqueue(currentNode->nodeName);
                 traversalStats.totalWeight += currentNode->weight;
                 visited[currentNode->nodeName - 'A'] = true;
+                trackTraversalPath(&traversalStats, currentNode->nodeName);
             }
 
             currentNode = currentNode->next;

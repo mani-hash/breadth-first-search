3aa8423b16b34dd9e958c75b1cde055d72d605a9
mani-hash <manimehalan400@gmail.com>
2025-01-25 13:32:12 +0530

    feat: main funtion logic

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "types.h"
+#include "decipherFile.h"
+#include "logic.h"
 
 int main()
 {
+    Graph *graph = createGraphFromFile();
+
+    displayDataFromTraversingGraph(graph);
+    
+    freeGraph(graph);
+    
     return 0;
 }
